86253a78044b97e33da48112466f0c7fe1d9c013
Kaatikaa <pados.kata@windowslive.com>
2026-05-15 09:42:20 +0200

    remove numpy
    nem is volt használva

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
